1d084f0c808bf6f29a1651619ff2fb217586f5fa
skartyman <skartyman@gmail.com>
2025-12-17 23:35:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.1.0"; // ← меняешь вручную при обновлениях
+const APP_VERSION = "1.1.2"; // ← меняешь вручную при обновлениях
 const SAVED_VERSION = localStorage.getItem("surp_version");
 
 if (SAVED_VERSION && SAVED_VERSION !== APP_VERSION) {
