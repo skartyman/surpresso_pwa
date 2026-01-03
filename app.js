8d1a888beec27a445cc95dd6c8b66690fda04037
skartyman <skartyman@gmail.com>
2026-01-03 19:39:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const USER_SHEET_ID  = "1TcDW8xV_-wdkBdK0FNCVmK-ZiHahnnsB9JsXvEUBA1s";
 const USER_SHEET_GID = 0;
 
 // Шаблоны наборов
-const TEMPLATES_FILE_ID = "1VwYf_Gy1iK1k6G7wi93FnpIZMHXAH8xjEmS1bsihh4E"; // JSON на Google Drive (чтение)
+const TEMPLATES_FILE_ID = "1b7msmOoFsJpQzyXpt7vsNKdxOpN_2kn3"; // JSON на Google Drive (чтение)https://drive.google.com/file/d/1b7msmOoFsJpQzyXpt7vsNKdxOpN_2kn3/view?usp=sharing
 const TEMPLATE_SAVE_WEBHOOK = "https://script.google.com/macros/s/AKfycbwK8g6vrhko8aXgSs46aJ_NJuSgxnLuhYX15i0Zqnj4Vo7iE43G4XHn5iD_s-3e5H_3/exec";
 
 let USERS = [];   // загруженные пользователи
@@ -2079,6 +2079,7 @@ attachSuggest(
 
   document.getElementById("new-btn").onclick = newInvoice;
 });
+
 
 
 
