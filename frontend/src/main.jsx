d5c4fb96f3519cc22d42d03d4bdface2d1e97ecf
skartyman <skartyman@gmail.com>
2026-04-02 09:41:23 +0300

    Revert "Revert "Integrate Telegram Mini App scaffold under /tg with /api/telegram and isolated uploads""

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './styles/app.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tg">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
